bcd9d2bef1a887b1aeb4a4eb14684fecdeedae09
Jacob Jagodzinski <jagodzinskijacob@gmail.com>
2022-06-19 17:40:15 -0500

    Created edit task fragment

---
+++ Qreminder/app/src/main/java/com/example/qreminder/edit_tasks.java
@@ -0,0 +1,75 @@
+package com.example.qreminder;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.qreminder.databinding.FragmentCustomTaskBinding;
+import com.example.qreminder.databinding.FragmentEditTasksBinding;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link edit_tasks#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class edit_tasks extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private FragmentEditTasksBinding binding;
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public edit_tasks() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment edit_tasks.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static edit_tasks newInstance(String param1, String param2) {
+        edit_tasks fragment = new edit_tasks();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentEditTasksBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
