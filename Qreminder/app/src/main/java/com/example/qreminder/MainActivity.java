da069686422638e53b07d27237a53c9b1723be67
Jacob Jagodzinski <jagodzinskijacob@gmail.com>
2022-06-23 18:26:40 -0500

    Fixed Stuff

--- Qreminder/app/src/main/java/com/example/qreminder/MainActivity.java
+++ Qreminder/app/src/main/java/com/example/qreminder/MainActivity.java
@@ -64,9 +64,11 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+       /* if (id == R.id.action_settings) {
             return true;
         }
+        */
+
 
         return super.onOptionsItemSelected(item);
     }
