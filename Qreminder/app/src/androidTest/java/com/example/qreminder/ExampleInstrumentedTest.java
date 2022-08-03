dc02d5846dd55bf4a8c85ff2b26d08de4b7f093f
Jacob Jagodzinski <jagodzinskijacob@gmail.com>
2022-08-03 11:46:56 -0500

    Update ExampleInstrumentedTest.java

--- Qreminder/app/src/androidTest/java/com/example/qreminder/ExampleInstrumentedTest.java
+++ Qreminder/app/src/androidTest/java/com/example/qreminder/ExampleInstrumentedTest.java
@@ -6,8 +6,15 @@ import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static androidx.test.espresso.assertion.ViewAssertions.*;
 
 import android.content.Context;
+import android.widget.Button;
 
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
+import androidx.test.espresso.Espresso;
 import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.RootMatchers;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -19,6 +26,11 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -26,6 +38,9 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -36,7 +51,55 @@ public class ExampleInstrumentedTest {
     @Test
     public void startClick() {
         onView(withId(R.id.startButton)).perform(click());
-        onView(withText("Add Task")).check(matches(isDisplayed()));
+        //onView(withText("Add Task")).check(matches(isDisplayed()));
 
     }
+
+    @Test
+    public void addTasks(){
+        onView(withId(R.id.startButton)).perform(click());
+        try{
+            Espresso.onView(ViewMatchers.withText("Edit Tasks")).perform(ViewActions.click());
+            onView(withId(R.id.floatingActionButton)).perform(click());
+            onView(withId(R.id.add_custom_task)).perform(click());
+
+        } catch(Exception e) {onView(withId(R.id.add_custom_task)).perform(click());}
+
+        int x = customTask((int) (Math.random()*100)+1);
+        onView(withId(R.id.add_tasks_done)).perform(click());
+
+
+    }
+
+    public int customTask(int x){
+        int i = 0;
+        List<String> choices = new ArrayList<String>();
+        choices.add("Everyday");
+        choices.add("Every 2 days");
+        choices.add("Every 5 days");
+        choices.add("Every 7 days");
+        choices.add("Every 10 days");
+        choices.add("Every 30 days");
+        choices.add("Every 45 days");
+        choices.add("Every 60 days");
+        choices.add("Every 90 days");
+        choices.add("Every 180 days");
+        choices.add("Every 365 days");
+
+
+        while (i<x) {
+            if (i!=0){onView(withId(R.id.add_custom_task)).perform(click());}
+
+            onView(withId(R.id.task_name)).perform(typeText("Task "+i));
+            onView(withId(R.id.task_name)).perform(closeSoftKeyboard());
+            onView(withId(R.id.reminder_dropdown)).perform(click());
+            onView(withText(choices.get((int)(Math.random()*choices.size()))))
+                    .inRoot(RootMatchers.isPlatformPopup())
+                    .perform(click());
+            onView(withId(R.id.createTaskDoneButton)).perform(click());
+            i++;
+        }
+    return x;
+    }
+
 }
