99b9c05425d039e0b9cb2ef870ec8712cf8d18f6
Megan Munro <meganmunro731@gmail.com>
2022-08-03 12:55:11 -0500

    Update ExampleInstrumentedTest.java

--- Qreminder/app/src/androidTest/java/com/example/qreminder/ExampleInstrumentedTest.java
+++ Qreminder/app/src/androidTest/java/com/example/qreminder/ExampleInstrumentedTest.java
@@ -103,6 +103,7 @@ public class ExampleInstrumentedTest {
     }
 
     @Test
+<<<<<<< Updated upstream
     public void changeScreen(){
         onView(withId(R.id.startButton)).perform(click());
         onView(withText("Overdue Tasks:")).check(matches(isDisplayed()));
@@ -115,6 +116,21 @@ public class ExampleInstrumentedTest {
 
 
 
+=======
+        public void changeScreen(){
+            onView(withId(R.id.startButton)).perform(click());
+            onView(withText("My Tasks")).check(matches(isDisplayed()));
+            Espresso.onView(ViewMatchers.withText("Edit Tasks")).perform(ViewActions.click());
+            onView(withText("Edit Tasks")).check(matches(isDisplayed()));
+            onView(withId(R.id.floatingActionButton)).perform(click());
+            onView(withText("Add Your Tasks +")).check(matches(isDisplayed()));
+            onView(withId(R.id.add_custom_task)).perform(click());
+            onView(withText("Custom Task")).check(matches(isDisplayed()));
+            onView(withId(R.id.createTaskDoneButton)).perform(click());
+            onView(withText("Add Your Tasks +")).check(matches(isDisplayed()));
+            onView(withId(R.id.add_tasks_done)).perform(click());
+            onView(withText("My Tasks")).check(matches(isDisplayed()));
+>>>>>>> Stashed changes
     }
 
 }
